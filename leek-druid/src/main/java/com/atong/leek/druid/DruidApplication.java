31a9516ce35c70fbce6f474c9e8c068c0bd1af52
atong <lt1550919167@163.com>
2020-12-07 19:58:18 +0800

    commit

--- leek-druid/src/main/java/com/atong/leek/druid/DruidApplication.java
+++ leek-druid/src/main/java/com/atong/leek/druid/DruidApplication.java
@@ -2,8 +2,9 @@ package com.atong.leek.druid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DruidApplication {
 
     public static void main(String[] args) {
