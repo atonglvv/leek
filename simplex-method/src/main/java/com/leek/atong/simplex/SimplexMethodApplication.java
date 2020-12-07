cfd55582b74c2d42aead86fc0bee57ddcc6287a9
atong <lt1550919167@163.com>
2020-12-07 17:56:46 +0800

    simplex-method[单纯形法|优化]

---
+++ simplex-method/src/main/java/com/leek/atong/simplex/SimplexMethodApplication.java
@@ -0,0 +1,13 @@
+package com.leek.atong.simplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplexMethodApplication.class, args);
+    }
+
+}
---
+++ simplex-method/src/test/java/com/leek/atong/simplex/SimplexMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leek.atong.simplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplexMethodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
