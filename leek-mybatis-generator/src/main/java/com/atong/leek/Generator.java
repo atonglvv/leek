cb5800c7bbc9ca43d673e166a3e45c9750942d44
atong <lt1550919167@163.com>
2020-12-17 19:36:57 +0800

    add mybatis generator module

---
+++ leek-mybatis-generator/src/main/java/com/atong/leek/Generator.java
@@ -0,0 +1,45 @@
+package com.atong.leek;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: leek
+ * @description: mybatis逆向工程代码生成器
+ * @author: atong
+ * @create: 2020-12-17 16:38
+ */
+public class Generator {
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("leek-mybatis-generator/src/main/resources/generatorConfig.xml");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.length());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Generator generator = new Generator();
+            generator.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
