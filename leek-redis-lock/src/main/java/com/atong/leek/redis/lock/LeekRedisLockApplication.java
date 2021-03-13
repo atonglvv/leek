0b5c382f30305741e1ca94a562152923da4b42ae
atongTs <atongLv@163.com>
2021-03-13 20:17:56 +0800

    leek-redis-lock

---
+++ leek-redis-lock/src/main/java/com/atong/leek/redis/lock/LeekRedisLockApplication.java
@@ -0,0 +1,14 @@
+package com.atong.leek.redis.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeekRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeekRedisLockApplication.class, args);
+    }
+
+
+}
---
+++ leek-redis-lock/src/test/java/com/atong/leek/redis/lock/LeekRedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atong.leek.redis.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeekRedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
