e675ec18f4d39332f3eff6411073a60629e697a2
atong <atongLv@163.com>
2022-07-15 17:55:42 +0800

    feat: JacksonConfig

--- leek-demo/src/main/java/com/atong/leek/demo/controller/HelloController.java
+++ leek-demo/src/main/java/com/atong/leek/demo/controller/HelloController.java
@@ -1,18 +1,21 @@
 package com.atong.leek.demo.controller;
 
+import com.atong.leek.demo.vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("seo")
 public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @GetMapping("/hello")
-    public String hello() {
-        logger.error("localhost:8080/hello is running...");
-        return "Hello World...";
+    @GetMapping("hello")
+    public User hello() {
+        logger.info("localhost:8080/hello is running...");
+        return new User();
     }
 }
---
+++ leek-demo/src/main/java/com/atong/leek/demo/vo/User.java
@@ -0,0 +1,19 @@
+package com.atong.leek.demo.vo;
+
+/**
+ * @program: leek
+ * @description:
+ * @author: atong
+ * @create: 2022-07-15 16:54
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
