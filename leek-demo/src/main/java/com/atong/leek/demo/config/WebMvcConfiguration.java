4e51f340395aff61a3038d73be38dff1f6ef764d
atong <atongLv@163.com>
2022-07-18 18:03:03 +0800

    feat: WebMvcConfigurationSupport

---
+++ leek-demo/src/main/java/com/atong/leek/demo/config/WebMvcConfiguration.java
@@ -0,0 +1,76 @@
+package com.atong.leek.demo.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: leek
+ * @description:
+ * @author: atong
+ * @create: 2022-07-18 13:59
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //删除springboot默认的StringHttpMessageConverter解析器
+        converters.removeIf(x -> x instanceof StringHttpMessageConverter);
+        for (HttpMessageConverter<?> converter : converters) {
+            // 解决 Controller 返回普通文本中文乱码问题
+            if (converter instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8);
+            }
+            // 解决 Controller 返回json对象中文乱码问题
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8);
+            }
+        }
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+                //json格式输出
+                SerializerFeature.PrettyFormat,
+                // 保留map为空的字段
+                SerializerFeature.WriteMapNullValue,
+                // 将String类型的null转成""形式
+                SerializerFeature.WriteNullStringAsEmpty,
+                // 将Number类型的null转成0
+                SerializerFeature.WriteNullNumberAsZero,
+                // 将List类型的null转成[],而不是""
+                SerializerFeature.WriteNullListAsEmpty,
+                // Boolean类型的null转成false
+                SerializerFeature.WriteNullBooleanAsFalse,
+                // 处理可能循环引用的问题
+                SerializerFeature.DisableCircularReferenceDetect);
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        mediaTypeList.add(MediaType.APPLICATION_JSON);
+        converter.setSupportedMediaTypes(mediaTypeList);
+        converters.add(converter);
+    }
+}
--- leek-demo/src/main/java/com/atong/leek/demo/controller/HelloController.java
+++ leek-demo/src/main/java/com/atong/leek/demo/controller/HelloController.java
@@ -4,18 +4,16 @@ import com.atong.leek.demo.vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("seo")
 public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("hello")
     public User hello() {
-        logger.info("localhost:8080/hello is running...");
+        logger.info("localhost:8090/hello is running...");
         return new User();
     }
 }
